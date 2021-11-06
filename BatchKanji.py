94ea0097b7e4b68cb653074e67b7915b1d1e99a6
Troy Loeffler <troydloeffler@gmail.com>
2021-11-06 12:41:01 -0700

    Added TTS features to the card creation:

--- BatchKanji.py
+++ BatchKanji.py
@@ -4,8 +4,10 @@ import ssl
 from difflib import SequenceMatcher
 import re
 import genanki
+from gtts import gTTS
 
-answerformat = '<div class=jp> {{furigana:Reading}} </div>\n <hr id=answer>\n<div class=jptiny align=right>({{Chapter Number}})</div>\n<div class=jp>{{Meaning}}</div>\n<p><span class=jpmid>{{furigana:Comment}}</span></p>'
+
+answerformat = '<div class=jp> {{furigana:Reading}} </div>\n{{Voice}} \n<hr id=answer>\n<div class=jp>{{Meaning}}</div>\n{{PartOfSpeach}} \n<p><span class=jpmid>{{furigana:Comment}}</span></p>'
 
 styling = '.card {font-family: arial;   font-size: 20px;    text-align: center;    color: black;    background-color: white;    }    .jp { font-size: 30px }    .win .jp { font-family: "MS Mincho", "ＭＳ 明朝"; }    .mac .jp { font-family: "Hiragino Mincho Pro", "ヒラギノ明朝 Pro"; }    .linux .jp { font-family: "TakaoPGothic"; }    .mobile .jp { font-family: myfont;}    .jpmid { font-size: 20px }    .win .jpmid { font-family: "MS Mincho", "ＭＳ 明朝"; }    .mac .jpmid { font-family: "Hiragino Mincho Pro", "ヒラギノ明朝 Pro"; }    .linux .jpmid { font-family: "TakaoPGothic";}    .mobile .jpmid { font-family: myfont; }    .jptiny { font-size: 10px }    .win .jptiny { font-family: "MS Mincho", "ＭＳ 明朝"; }    .mac .jptiny { font-family: "Hiragino Mincho Pro", "ヒラギノ明朝 Pro"; }    .linux .jptiny { font-family: "TakaoPGothic";}    .mobile .jptiny { font-family: myfont; }    @font-face { font-family: myfont; src: url("_TakaoPGothic.ttf"); }'
 
@@ -25,14 +27,14 @@ kanji_full = r'[㐀-䶵一-鿋豈-頻]+'
 #radicals = r'[⺀-⿕]'
 #katakana_half_width = r'[｟-ﾟ]'
 
-
-#This defines the card formatting that will be created.  
 japanwordsmodel = genanki.Model(160227392319, 'Japanese Word Recognition',css=styling,
               fields=[
                       {'name': 'Expression'},
                       {'name': 'Reading'},
                       {'name': 'Meaning'},
-#                      {'name': 'Comment'},
+                      {'name': 'Voice'},
+                      {'name': 'PartOfSpeach'},
+                      {'name': 'Comment'},
                             ],
                 templates=[
 
@@ -45,20 +47,24 @@ japanwordsmodel = genanki.Model(160227392319, 'Japanese Word Recognition',css=st
                           'qfmt': '<div class=jp> {{Expression}} </div>',
                           'afmt': answerformat,
                           'styling': styling},
+                 {'name': 'Voice -> Meaning', 
+                          'qfmt': '<div class=jp> {{Voice}}\n {{PartOfSpeach}} </div>',
+                          'afmt': answerformat,
+                          'styling': styling},
                           ]
                 )
 
 
-#========================================================================================
+
 def batchcreate(srcTxt, commononly=True):
     searchtxt = srcTxt.strip()
     my_deck = genanki.Deck(
-                 2059400110,
-                'Node Kanji'
-                )
+              2059400110,
+                'Node Kanji')
 
+    print("Common Only: {}".format(commononly))
+    mediafiles = []
 
-    #Itterates over the characters in the
     for char in searchtxt:
         # result holders for API call
         word = ''
@@ -73,19 +79,25 @@ def batchcreate(srcTxt, commononly=True):
         
         baseUrl = 'http://jisho.org/api/v1/search/words?keyword='
 
-        print("Common Only: {}".format(commononly))
-        print(char)
 
+        hiraganacheck = extract_unicode_block(hiragana_full, char)
+        if len(hiraganacheck) > 0:
+            print("Character %s is Hiragana, skipping..."%(char))
+            continue
+
+        katakanacheck = extract_unicode_block(katakana_full, char)
+        if len(katakanacheck) > 0:
+            print("Character %s is Katakana, skipping..."%(char))
+            continue
+        print(char)
+        searchStringsingle = char
         searchString = "*"+char+"*"
         if commononly:
+            searchStringsingle = char + " #common"
             searchString = searchString + " #common"
 
 #        searchString = searchString + " #kanji"
         print("Searching Jisho with the string: {}".format(searchString))
-
-        #The following code is modified from Nick Youngblood's <osunightfall@gmail.com>
-        #Anki extention. 
-        # See http://ankiweb.net/shared/info/1545080191 for original extention code.
 
         # Make url conform to ASCII 
         encodedUrl = baseUrl + urllib.parse.quote(searchString.encode('utf8'))
@@ -94,11 +106,10 @@ def batchcreate(srcTxt, commononly=True):
             response = urllib.request.urlopen(encodedUrl, context=context).read()
             parsed_json = json.loads(response.decode('utf-8'))
         except IOError as e:
-            return 
-
+            return
 
         # Make url conform to ASCII 
-        encodedUrl = baseUrl + urllib.parse.quote(searchString.encode('utf8'))
+        encodedUrl = baseUrl + urllib.parse.quote(searchStringsingle.encode('utf8'))
         context = ssl._create_unverified_context()
         try:
             response = urllib.request.urlopen(encodedUrl, context=context).read()
@@ -106,121 +117,148 @@ def batchcreate(srcTxt, commononly=True):
         except IOError as e:
             return 
 
-        parsed_json.update(parsed_json2)
+        jsonlist = [parsed_json2, parsed_json]
+
+#        print(parsed_json)
+#        quit()
 
 
 
         # Exit if nothing useful came back
-        try: 
-            responseHasResults = parsed_json['data'][0]
-        except IndexError:
-            print("Jisho.org returned no data for search term '%s'"%(searchString))
-            return 
+#        try: 
+#            responseHasResults = parsed_json['data'][0]
+#            responseHasResults = parsed_json2['data'][0]
+#        except IndexError:
+#            try:
+#                responseHasResults = parsed_json2['data'][0]
+#            except IndexError:
+#                    print("Jisho.org returned no data for search term '%s'"%(searchString))
+#                    return 
 
-        for word in parsed_json["data"]:
+        for curjson in jsonlist:
+            #Check to see if data was returned from this search.
+            try:
+                curjson['data'][0]
+            except IndexError:
+                continue
+            for word in curjson["data"]:
 
-            try: # Get proper word if it was typed with kana and should have kanji, or 
-                properspelling = word['japanese'][0]['word']
-            except (IndexError, KeyError):
-                try: # Katakana search terms put the word elsewhere
-                    properspelling = word['japanese'][0]['reading']
+                try: # Get proper word if it was typed with kana and should have kanji, or 
+                    properspelling = word['japanese'][0]['word']
+                except (IndexError, KeyError):
+                    try: # Katakana search terms put the word elsewhere
+                        properspelling = word['japanese'][0]['reading']
+                    except (IndexError, KeyError):
+                        pass
+
+                if char not in properspelling:
+                    continue
+
+                # Get the first definition if it exists
+                try:
+                    definitionList = word['senses'][0]['english_definitions']
+                    definition = ''
+                    for defWord in definitionList:
+                        if (len(definition) > 0):
+                            definition = definition + ', '
+                        definition = definition + defWord
+             
                 except (IndexError, KeyError):
                     pass
 
+                # Get the second definition if it exists
+                try:
+                    definition2List = word['senses'][1]['english_definitions']
+                    for defWord in definition2List:
+                        if (len(definition2) > 0):
+                            definition2 = definition2 + ', '
+                        definition2 = definition2 + defWord
+                except (IndexError, KeyError):
+                    pass
 
-            # Get the first definition if it exists
-            try:
-                definitionList = word['senses'][0]['english_definitions']
-                definition = ''
-                for defWord in definitionList:
-                    if (len(definition) > 0):
-                        definition = definition + ', '
-                    definition = definition + defWord
-         
-            except (IndexError, KeyError):
-                pass
+                # Get the part of speech string if it exists
+                try:
+                    partOfSpeech = word['senses'][0]['parts_of_speech'][0]
+                except (IndexError, KeyError):
+                    pass    
 
-            # Get the second definition if it exists
-            try:
-                definition2List = word['senses'][1]['english_definitions']
-                for defWord in definition2List:
-                    if (len(definition2) > 0):
-                        definition2 = definition2 + ', '
-                    definition2 = definition2 + defWord
-            except (IndexError, KeyError):
-                pass
+                # Get the reading string if it exists
+                reading = None
+                try:
+                    reading = word['japanese'][0]['reading']
+                except (IndexError, KeyError):
+                    pass    
+                #If there's a reading, use TTS to generate an audio file.
+                if reading is not None:
+                    tts = gTTS(reading, lang='ja')
+                    soundfile = "%s.mp3"%(properspelling)
+                    mediafiles.append(soundfile)
+                    tts.save(soundfile)
+                    soundstring = "[sound:%s]"%(soundfile)
+                else:
+                    soundstring = ""
 
-            # Get the part of speech string if it exists
-            try:
-                partOfSpeech = word['senses'][0]['parts_of_speech'][0]
-            except (IndexError, KeyError):
-                pass    
+                # Get the tags string if it exists
+                try:
+                    tags = word['senses'][0]['tags'][0]
+                except (IndexError, KeyError):
+                    pass    
 
-            # Get the reading string if it exists
-            try:
-                reading = word['japanese'][0]['reading']
-            except (IndexError, KeyError):
-                pass    
+                # Get the restrictions string if it exists
+                try:
+                    restrictions = word['senses'][0]['restrictions'][0]
+                except (IndexError, KeyError):
+                    pass    
 
-            # Get the tags string if it exists
-            try:
-                tags = word['senses'][0]['tags'][0]
-            except (IndexError, KeyError):
-                pass    
+                # Get the antonyms string if it exists
+                try:
+                    antonyms = word['senses'][0]['antonyms'][0]
+                except (IndexError, KeyError):
+                    pass    
 
-            # Get the restrictions string if it exists
-            try:
-                restrictions = word['senses'][0]['restrictions'][0]
-            except (IndexError, KeyError):
-                pass    
+                # Get the info string if it exists
+                try:
+                    info = word['senses'][0]['info'][0]
+                except (IndexError, KeyError):
+                    pass
 
-            # Get the antonyms string if it exists
-            try:
-                antonyms = word['senses'][0]['antonyms'][0]
-            except (IndexError, KeyError):
-                pass    
+                print(partOfSpeech)
 
-            # Get the info string if it exists
-            try:
-                info = word['senses'][0]['info'][0]
-            except (IndexError, KeyError):
-                pass
+                hiragana = extract_unicode_block(hiragana_full, properspelling)
+                katakana = extract_unicode_block(katakana_full, properspelling)
+                kanji = extract_unicode_block(kanji_full, properspelling)
+                rawspelling = properspelling
+                newstr = reading
+                for substr in sorted(hiragana, reverse=True):
+                    newstr = re.sub(substr, " ", newstr)
+                for substr in sorted(katakana, reverse=True):
+                    newstr = re.sub(substr, " ", newstr)
+                for substr in sorted(kanji, reverse=True):
+                    properspelling = re.sub(substr, " "+substr+"%s", properspelling)
 
-            #This code is designed to automatically generate the furigana strings
-            #and place them at the proper location. IE if you have a word
-            #with a hiragana tail, it will place the furigana before the tail
-            #so that it appears above the Kanji in question. There may
-            #be a few bugs with this, but so far it works for a lot of basic words.
-            hiragana = extract_unicode_block(hiragana_full, properspelling)
-            katakana = extract_unicode_block(katakana_full, properspelling)
-            kanji = extract_unicode_block(kanji_full, properspelling)
-            rawspelling = properspelling
-            newstr = reading
-            for substr in sorted(hiragana, reverse=True):
-                newstr = re.sub(substr, " ", newstr)
-            for substr in sorted(katakana, reverse=True):
-                newstr = re.sub(substr, " ", newstr)
-            for substr in sorted(kanji, reverse=True):
-                properspelling = re.sub(substr, substr+"%s", properspelling)
-
-            furigana = ["["+x+"]" for x in newstr.split()]
+                furigana = ["["+furi+"]" for furi in newstr.split()]
 
 #            print(properspelling)
 #            print(furigana)
-            try:
-                outreading = properspelling%(tuple(furigana))
-            except:
                 try:
-                    outreading = properspelling%("["+reading+"]")
+                    outreading = properspelling%(tuple(furigana))
                 except:
-                    continue
-
-            print(outreading)
-            my_note = genanki.Note(
-                    model=japanwordsmodel,
-                    fields=[properspelling.replace("%s", ""), outreading, definition])
-            my_deck.add_note(my_note)
-        genanki.Package(my_deck).write_to_file('output.apkg')
+                    try:
+                        outreading = properspelling%("["+reading+"]")
+                    except:
+                        continue
+                print(outreading.replace(" ", ""))
+                my_note = genanki.Note(
+                        model=japanwordsmodel,
+                        fields=[properspelling.replace("%s", ""), outreading, definition, 
+                                soundstring,partOfSpeech,''])
+                my_deck.add_note(my_note)
+        package = genanki.Package(my_deck)
+        package.media_files = mediafiles
+        package.write_to_file('output.apkg')
+        for media in mediafiles:
+            if os.path.exists(media):
+                os.remove(media)
 
 
 #===============================================================================
